fa9124b982b01bdc0140b99ca40dc0a4d6c3bf37
abdulazeez761 <75909460+abdulazeez761@users.noreply.github.com>
2023-11-15 17:27:09 +0300

    Update generateAndSendEmailcontroller.js

--- controllers/generateAndSendEmailcontroller.js
+++ controllers/generateAndSendEmailcontroller.js
@@ -3,44 +3,46 @@ const qr = require('qr-image');
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./model/dataBase.db');
 exports.generateAndSendEmail = async (req, res) => {
-    const userInfo = req.body;
-    const sql = `
+  const userInfo = req.body;
+  const sql = `
         INSERT INTO users (username, company,email, phone, job_title, date)
         VALUES (?, ?, ?, ?, ?, ? )
     `;
 
-    db.all(`SELECT * FROM users WHERE email = ?`, [userInfo.email], (err, rows) => {
-        if (rows.length == 0) {
-            db.run(sql, [userInfo.name, userInfo.company, userInfo.email, userInfo.phone, userInfo.JobTitle, userInfo.date], function (err) {
-                if (err) {
-                    return console.error(err.message);
-                }
-            });
+  db.all(`SELECT * FROM users WHERE email = ?`, [userInfo.email], (err, rows) => {
+    if (rows.length == 0) {
+      db.run(sql, [userInfo.name, userInfo.company, userInfo.email, userInfo.phone, userInfo.JobTitle, userInfo.date], function (err) {
+        if (err) {
+          return console.error(err.message);
         }
-    });
+      });
+    }
+  });
 
 
-    const jsonString = JSON.stringify(userInfo);
-    let cleanedString = jsonString
-        .replace(/[\{\}\"]/g, '')
-        .replace(/,/g, ',\n')
-        .replace(/:/g, ': ');
-    const qrCode = qr.image(cleanedString, { type: 'png' });
+  const jsonString = JSON.stringify(userInfo);
+  let cleanedString = jsonString
+    .replace(/[\{\}\"]/g, '')
+    .replace(/,/g, ',\n')
+    .replace(/:/g, ': ');
+  const qrCode = qr.image(cleanedString, { type: 'png' });
 
-    const transporter = nodemailer.createTransport({
-        service: 'Gmail', // Use your email provider here
-        auth: {
-            user: 'abd828981@gmail.com',
-            pass: 'ucyt swab mnhk xxkh',
-        },
-    });
+  const transporter = nodemailer.createTransport({
+    host: "pro.eu.turbo-smtp.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "event@viennaadvantage.com",
+      pass: "3GqneS1J",
+    },
+  });
 
-    // Send an email with the QR code embedded in the HTML content
-    const mailOptions = {
-        from: 'abd828981@gmail.com',
-        to: userInfo.email,
-        subject: 'Your QR Code',
-        html: `
+  // Send an email with the QR code embedded in the HTML content
+  const mailOptions = {
+    from: 'event@viennaadvantage.com',
+    to: userInfo.email,
+    subject: 'Your QR Code',
+    html: `
            
 <!DOCTYPE html>
 <html lang="en">
@@ -127,90 +129,23 @@ exports.generateAndSendEmail = async (req, res) => {
 </html>
 
         `,
-        attachments: [
-            {
-                filename: 'qrcode.png',
-                content: qrCode, // Provide the QR code data URL here
-                encoding: 'base64',
-            },
-        ],
-    };
+    attachments: [
+      {
+        filename: 'qrcode.png',
+        content: qrCode, // Provide the QR code data URL here
+        encoding: 'base64',
+      },
+    ],
+  };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.error('Error sending email:', error);
-            res.status(500).json({ message: 'Email could not be sent.' });
-        } else {
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Error sending email:', error);
+      res.status(500).json({ message: 'Email could not be sent.' });
+    } else {
 
-            res.json({ message: 'Email sent successfully.' });
-        }
-    });
+      res.json({ message: 'Email sent successfully.' });
+    }
+  });
 
 }
-
-
-/*
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Vienna Advantage Arabia Event</title>
-  <style>
-    body {
-      font-family: 'Arial', sans-serif;
-      background-color: #f5f5f5;
-      margin: 0;
-      padding: 0;
-    }
-
-    .container {
-      max-width: 600px;
-      margin: 30px auto;
-      background-color: #ffffff;
-      padding: 20px;
-      border-radius: 8px;
-      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    }
-
-    h1 {
-      color: #333333;
-    }
-
-    p {
-      color: #555555;
-    }
-
-    .button {
-      display: inline-block;
-      padding: 10px 20px;
-      text-align: center;
-      text-decoration: none;
-      background-color: blue;
-      color: #ffffff;
-      border-radius: 5px;
-      transition: background-color 0.3s ease;
-    }
-
-    .button:hover {
-      background-color: #00308F;
-    }
-
-    .footer {
-      margin-top: 20px;
-      text-align: center;
-      color: #888888;
-    }
-  </style>
-</head>
-<body>
-  <div class="container">
-    <h1>Vienna Advantage Arabia Event</h1>
-    <p>We are thrilled to announce the launch of our first-ever Vienna Advantage Arabia Event and are excited to have you join us.</p>
-    <p>Your presence at this event is invaluable, and we can't wait to share this remarkable experience with you.</p>
-    <p>If you encounter any issues or have questions, feel free to reach out.</p>
-    <a href="https://invitationwebsitefrontend-production.up.railway.app/" class="button">Confirm Registration</a>
-    <p class="footer">Looking forward to seeing you at the VA Arabia Event!<br>Best regards,</p>
-  </div>
-</body>
-</html>
- */
